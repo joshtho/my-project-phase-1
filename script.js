e55628f75359582e4b633e05c3950bc53ad4c9e5
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-05-19 21:48:21 -0500

    added fetch and building first event listeners

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 const searchBar = document.getElementById('myInput')
-const submitBtn = document.querySelector('.submit')
+const submitBtn = document.querySelector('.container')
 submitBtn.addEventListener('submit', (event) => {
+    let playerName = event.target[0].value
+    
     event.preventDefault()
-    fetchAllPlayers()
+    fetchAllPlayers(event)
 })
-
+function findPlayer(players) {
+    return players.first_name && players.last_name == `${playerName}`
+}
 // Event Handlers 
 // const firstFindMyPlayer (event) => {
-//         event.preventDefault()
-//         console.log(event)
-//         fetch('https://www.balldontlie.io/api/v1/players')
-//         .then(response => response.json())
-//         .then(data => {
-//             const searchString = event.target.value;
-//             if(searchString == `${data.full_name && data.last_name}`) {
-//                 return createCard(player)
-//             }
-//         })
-//     }
-// Fetch Functions
-function fetchAllPlayers(event) {
-    console.log(event)
-    fetch('https://www.balldontlie.io/api/v1/players')
-        .then(response => response.json())
-        .then(data => console.log(data))
+    //         event.preventDefault()
+    //         console.log(event)
+    //         fetch('https://www.balldontlie.io/api/v1/players')
+    //         .then(response => response.json())
+    //         .then(data => {
+        //             const searchString = event.target.value;
+        //             if(searchString == `${data.full_name && data.last_name}`) {
+            //                 return createCard(player)
+            //             }
+            //         })
+            //     }
+            // Fetch Functions
+            function fetchAllPlayers(event) {
+                console.log(event)
+                fetch('https://www.balldontlie.io/api/v1/players')
+                .then(response => response.json())
+                .then(data => {
+                    
+                }
+
+            })
 }
 // function findMyPlayer(object) {
 //     object.find(foundPlayer)
